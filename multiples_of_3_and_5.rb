065666605f00f1fe16114c9a429e218ff5f5f825
alexanderspace100 <alexanderspace100@gmail.com>
2016-08-25 19:59:49 +0300

    another solution for problem1

--- multiples_of_3_and_5.rb
+++ multiples_of_3_and_5.rb
@@ -4,6 +4,13 @@
 
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
+
+
+# 1.
+# (0..999).inject{ |sum, n| n%3==0||n%5==0 ? sum : sum+n }
+# or:
+# 2 below
+
 def multiples_of_3_and_5(range:)
 	range.select { |number| number % 3 == 0 || number % 5 == 0 }.reduce(:+)
 end
